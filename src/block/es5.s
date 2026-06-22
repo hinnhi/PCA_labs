cd1178d46f33f5441e19a524a3860484ab309c0f
Michele Missiroli <163904638+hinnhi@users.noreply.github.com>
2026-06-23 00:03:38 +0200

    Create es5.s

---
+++ src/block/es5.s
@@ -0,0 +1,20 @@
+.global _start
+_start:
+
+	#uit64_t x[3] = {1, 2, 3};
+	
+	la a5,x
+	ld a4,16(a5)
+	ld a5,8(a5)
+	add a4,a4,a5
+	ld a5,x
+	add a5,a4,a5
+	li a1,3
+	mv a0,a5
+	call __univdi3
+	mv a5,a0
+	mv a4,a5
+	la a5,x
+	sd a4,16(a5)
+	
+	#uint64_t x[3] = {1, 2, 2};
