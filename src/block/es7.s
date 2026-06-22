51c6acd90b630b68f8950d56ec75f478bd8195c5
Michele Missiroli <163904638+hinnhi@users.noreply.github.com>
2026-06-23 01:29:34 +0200

    Create es7.s

---
+++ src/block/es7.s
@@ -0,0 +1,21 @@
+.global _start
+_start:
+	
+	la t0,v
+	blez a1,else
+	sd a2,0(t0)
+	
+else:
+	sd a3,0(t0)
+	
+#uint64_t v
+#
+#int main(){
+#	/* ... */
+#	if(a > 0) {
+#		v = b;
+#	} esle {
+#		v = c;
+#	}
+#	/* ... */
+#}
